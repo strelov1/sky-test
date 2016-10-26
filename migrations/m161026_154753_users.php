c848c003f65ccde9c06279eec4774ff71e56a551
strelov1 <strelov1@gmail.com>
2016-10-26 15:50:05 +0000

    init

---
+++ migrations/m161026_154753_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161026_154753_users extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m161026_154753_users cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
